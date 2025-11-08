ad64aebecb8538fee4edbfae9069a84ca320f94e
aasish limbu <146429145+aasishdeveloper@users.noreply.github.com>
2025-11-08 14:42:30 +0545

    Fix NepaliDatePicker import in JoinForm - change from named to default import

--- components/user/JoinForm.js
+++ components/user/JoinForm.js
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { NepaliDatePicker } from '../admin/event-calendar/NepaliDatePicker';
+import NepaliDatePicker from '../admin/event-calendar/NepaliDatePicker';
 
 const JoinForm = () => {
   const router = useRouter();
