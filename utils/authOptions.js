ed9dd951bcd19643ae70c0be2fbf24c21e17c569
aasish limbu <146429145+aasishdeveloper@users.noreply.github.com>
2025-12-08 14:24:31 +0545

    fix: add redirect callback to handle callbackUrl properly

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -184,6 +184,14 @@ export const authOptions = {
         return session;
       }
     },
+
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
   },
 
   secret: process.env.NEXTAUTH_SECRET,
