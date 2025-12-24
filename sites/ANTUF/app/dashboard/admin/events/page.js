377fcd48a079f3a9b1757033791101b06e263958
aasish limbu <146429145+aasishdeveloper@users.noreply.github.com>
2025-12-24 18:28:01 +0545

    fix: middleware blocking login redirects

--- sites/ANTUF/app/dashboard/admin/events/page.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import EventCalendarAdmin from "@/components/admin/event-calendar/EventCalendarAdmin";
-
-export default function AdminEventCalendarPage() {
-  return <EventCalendarAdmin />;
-}
